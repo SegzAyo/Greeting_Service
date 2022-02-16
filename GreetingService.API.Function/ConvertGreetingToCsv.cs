5cfd0c305bd325f7d39f57e25e1b4bbc92334256
Ayodele Segun (BT) <segun.ayodele@hm.com>
2022-02-16 13:47:23 +0100

    BlobGreetingRepository created

--- GreetingService.API.Function/ConvertGreetingToCsv.cs
+++ GreetingService.API.Function/ConvertGreetingToCsv.cs
@@ -1,24 +1,28 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
-using GreetingService.API.Client;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
-using Microsoft.Extensions.Logging;
+//using System;
+//using System.IO;
+//using System.Text;
+//using System.Text.Json;
+//using System.Threading.Tasks;
+//using GreetingService.Core.Entities;
+//using Microsoft.Azure.WebJobs;
+//using Microsoft.Azure.WebJobs.Host;
+//using Microsoft.Extensions.Logging;
 
-namespace GreetingService.API.Function
-{
-    public class ConvertGreetingToCsv
-    {
-        [FunctionName("ConvertGreetingToCsv")]
-        public async Task Run([BlobTrigger("samples-workitems/{name}", Connection = "SegBlobConnectionString")] Stream greetingJsonBlob, string name, [Blob("greetings-csv/{name}", FileAccess.Write, Connection = "LoggingStorageAccount")] Stream greetingCsvBlob, ILogger log)
-        {
-            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {greetingJsonBlob.Length} Bytes");
 
-            var streamWriter = new StreamWriter(greetingCsvBlob);       
-            streamWriter.WriteLine("id;from;to;message;timestamp");     
-            streamWriter.WriteLine($"{Greeting.Id};{greeting.from};{greeting.To};{greeting.Message};{greeting.Timestamp}");  
-            await streamWriter.FlushAsync();
-        }
-    }
-}
+//namespace GreetingService.API.Function
+//{
+//    public class ConvertGreetingToCsv
+//    {
+//        [FunctionName("ConvertGreetingToCsv")]
+//        public async Task Run([BlobTrigger("greetings/{name}", Connection = "SegBlobConnectionString")] Stream greetingJsonBlob, string name, [Blob("greetings-csv/{name}", FileAccess.Write, Connection = "SegBlobConnectionString")] Stream greetingCsvBlob, ILogger log)
+//        {
+//            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {greetingJsonBlob.Length} Bytes");
+
+//            var greeting = JsonSerializer.Deserialize<Greeting>(greetingJsonBlob);
+//            var streamWriter = new StreamWriter(greetingCsvBlob);       
+//            streamWriter.WriteLine("id;from;to;message;timestamp");     
+//            streamWriter.WriteLine($"{greeting.Id};{greeting.From};{greeting.To};{greeting.Message};{greeting.Timestamp}");  
+//            await streamWriter.FlushAsync();
+//        }
+//    }
+//}
